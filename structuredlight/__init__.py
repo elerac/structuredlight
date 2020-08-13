64af2b8aa449d5784e9516a61435bc05dbf3194c
elerac <maenosuke079@gmail.com>
2020-08-13 17:42:24 +0900

    add phase-shifting code

--- structuredlight/__init__.py
+++ structuredlight/__init__.py
@@ -3,5 +3,9 @@ from .structuredlight import StructuredLight
 from .binary import Binary
 from .gray import Gray
 from .xor import XOR
+
+from .phaseshifting import PhaseShifting
+
 from .stripe import Stripe
+
 from .checker import Checker
