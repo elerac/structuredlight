af8ee4048d1c2bf527f816f0d9b2e13bfd7fa3da
elerac <maenosuke079@gmail.com>
2020-08-13 18:06:46 +0900

    add ramp code

--- structuredlight/__init__.py
+++ structuredlight/__init__.py
@@ -4,6 +4,7 @@ from .binary import Binary
 from .gray import Gray
 from .xor import XOR
 
+from .ramp import Ramp
 from .phaseshifting import PhaseShifting
 
 from .stripe import Stripe
---
+++ structuredlight/ramp.py
@@ -0,0 +1,25 @@
+import numpy as np
+from structuredlight import StructuredLight
+
+class Ramp(StructuredLight):
+    def __init__(self):
+        self.width = None
+
+    def generate(self, dsize):
+        width, height = dsize
+        self.width = width
+
+        img_white = 255*np.ones((height, width), dtype=np.uint8)
+        img_ramp  = (255*np.fromfunction(lambda y,x: x/(width-1), (height,width), dtype=float)).astype(np.uint8)
+        
+        patternImages = [img_white, img_ramp]
+        return patternImages
+
+    def decode(self, patternImages):
+        img_white = patternImages[0]
+        img_ramp  = patternImages[1]
+
+        img_ratio = img_ramp/img_white
+        
+        img_index = img_ratio*(self.width-1)
+        return img_index
