a78413fd5010bd5c20553b3ffcab448a40661a1d
elerac <maenosuke079@gmail.com>
2020-08-14 23:55:16 +0900

    Update phaseshifting.py

--- structuredlight/phaseshifting.py
+++ structuredlight/phaseshifting.py
@@ -4,11 +4,15 @@ from structuredlight import StructuredLight
 class PhaseShifting(StructuredLight):
     def __init__(self, num=3):
         self.num = num
+        self.width = None
+        self.F = 1.0
     
     def generate(self, dsize):
         width, height = dsize
         num = self.num
-        w = 2*np.pi/width
+        self.width= width
+
+        w = 2*np.pi/width * self.F
 
         imgs_code = (255*np.fromfunction(lambda y,x,n: 0.5*(np.cos(w*x + 2*np.pi*n/num) + 1), (height,width,num), dtype=float)).astype(np.uint8)
         
@@ -16,6 +20,31 @@ class PhaseShifting(StructuredLight):
         return imlist
 
     def decode(self, imlist):
+        img_phase = self.decodePhase(imlist)
+        w = 2*np.pi/self.width * self.F
+        img_index = img_phase/w
+        return img_index
+
+    def decodePhase(self, imlist):
+        U = self.__calcParam(imlist)
+        U1, U2, U3 = self.split(U)
+        img_phase = np.arccos(U2/np.sqrt(U2**2 + U3**2))
+        img_phase[U3<0] = 2*np.pi - img_phase[U3<0]
+        return img_phase
+
+    def decodeAmplitude(self, imlist):
+        U = self.__calcParam(imlist)
+        U1, U2, U3 = self.split(U)
+        img_amplitude = np.sqrt(U2**2 + U3**2)
+        return img_amplitude
+
+    def decodeOffset(self, imlist):
+        U = self.__calcParam(imlist)
+        U1, U2, U3 = self.split(U)
+        img_offset = U1
+        return img_offset
+    
+    def __calcParam(self, imlist):
         num = len(imlist)
         
         n = np.arange(0, num)
@@ -23,10 +52,4 @@ class PhaseShifting(StructuredLight):
         M = np.array([np.ones_like(n), np.cos(2*np.pi*n/num), -np.sin(2*np.pi*n/num)]).T #(num, 3)
         M_pinv = np.linalg.inv(M.T @ M) @ M.T #(3, num)
         U = np.tensordot(M_pinv, R, axes=(1,2)).transpose(1, 2, 0) #(height, width, 3)
-        
-        U1, U2, U3 = self.split(U)
-        A = np.sqrt(U2**2 + U3**2)
-        img_phase = np.arccos(U2/A)
-        img_phase[U3<0] = 2*np.pi - img_phase[U3<0]
-        
-        return img_phase
+        return U
