ad5fe356df87250a62a036dd2bc1aa53d66d0a83
elerac <maenosuke079@gmail.com>
2020-08-15 22:21:09 +0900

    Update export_all.py

--- examples/export_all.py
+++ examples/export_all.py
@@ -8,16 +8,20 @@ import structuredlight as sl
 def main():
     width = 320
     height = 240
-    slins_list = [sl.Binary(), sl.Gray(), sl.XOR(), sl.Ramp(), sl.PhaseShifting(), sl.Stripe()]
+    # Structued Light instance list
+    slins_list  = [sl.Binary(), sl.Gray(), sl.XOR(), sl.Ramp(), sl.PhaseShifting(), sl.Stripe()]
+    # Structure Light name list
     slname_list = ["binary", "gray", "xor", "ramp", "phaseshifting", "stripe"]
 
     for slins, slname in zip(slins_list, slname_list):
         imlist = slins.generate((width, height))
         
+        # Make directory if it doesn't exist
         dirname = slname + "{0}x{1}".format(width, height)
         if os.path.isdir(dirname) == False:
             os.makedirs(dirname)
-
+        
+        # Export images
         zero_pad = len(str(len(imlist)))
         for i, img in enumerate(imlist):
             filename = slname + str(i+1).zfill(zero_pad) + ".png"
