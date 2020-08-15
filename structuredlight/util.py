80c539f7e98e9372dc509a1ac017a6fa637e05cd
elerac <maenosuke079@gmail.com>
2020-08-15 16:09:54 +0900

    Update util.py

--- structuredlight/util.py
+++ structuredlight/util.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 def transpose(imlist):
+    """
+    Replacing the x-axis and y-axis of an image in the list of images
+    """
     return [ img.T for img in imlist]
 
 def invert(imlist):
+    """
+    Invert the color of an image in the list of images
+    """
     return [ 255-img for img in imlist]
