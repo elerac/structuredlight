e70b383e81fc6c9ada5830543e6fa00b5a85efc9
elerac <maenosuke079@gmail.com>
2020-08-15 18:36:13 +0900

    Create capture_x.py

---
+++ examples/capture_x.py
@@ -0,0 +1,47 @@
+"""
+Capture projection pattern and decode x-coorde.
+"""
+import cv2
+import numpy as np
+import structuredlight as sl
+
+def imshowAndCapture(cap, img_pattern, delay=250):
+    cv2.imshow("", img_pattern)
+    cv2.waitKey(delay)
+    ret, img_frame = cap.read()
+    img_gray = cv2.cvtColor(img_frame, cv2.COLOR_BGR2GRAY)
+    return img_gray
+
+def main():
+    width  = 640
+    height = 480
+
+    cap = cv2.VideoCapture(0) # External web camera
+    gray = sl.Gray()
+   
+    # Generate and Decode x-coorde
+    # Generate
+    imlist_posi_pat = gray.generate((width, height))
+    imlist_nega_pat = sl.invert(imlist_posi_pat)
+
+    # Capture
+    imlist_posi_cap = [ imshowAndCapture(cap, img) for img in imlist_posi_pat]
+    imlist_nega_cap = [ imshowAndCapture(cap, img) for img in imlist_nega_pat]
+    
+    # Decode
+    img_index = gray.decode(imlist_posi_cap, imlist_nega_cap)
+
+    # Write correspondence table
+    corr_table = sl.getCorrespondenceTable(img_index)
+    np.savetxt("correspondence.csv", corr_table, delimiter=",")
+    
+    # Visualize decode result
+    img_correspondence = np.clip(img_index/width*255.0, 0, 255).astype(np.uint8)
+    cv2.imshow("corresponnence map", img_correspondence)
+    cv2.waitKey(0)
+    cv2.imwrite("correspondence.png", img_correspondence)
+    cv2.destroyAllWindows()
+    cap.release()
+
+if __name__=="__main__":
+    main()
