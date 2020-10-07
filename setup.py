38e9c884470e47cec5a2639afaa47f7db925a04d
elerac <maenosuke079@gmail.com>
2020-10-07 20:23:59 +0900

    version 1.0.0 -> 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='structuredlight',
-    version='1.0.0',
+    version='1.0.1',
     description='Generate and Decode structured light',
     url='https://github.com/elerac/structuredlight',
     author='Ryota Maeda',
